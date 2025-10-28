aceb7284578ed9de680d427d893fef5a21cb10e4
TNCuong123 <yuriismylife123@gmail.com>
2025-10-28 15:09:11 +0700

    Add files via upload

---
+++ Client/Schedule/UCDays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NT106_BT2.Schedule
+{
+    public partial class UCDays : UserControl
+    {
+        public UCDays()
+        {
+            InitializeComponent();
+        }
+
+        private void UCDays_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void days(int numday)
+        {
+            IBdays.Text = numday + "";
+        }
+
+    }
+}
