bce7cd0d47b908d7a42fda4ccdf1a07312becf41
huyhieu <huyhieu@DESKTOP-BG9SCI0>
2025-12-17 19:23:10 +0700

    sdad

--- Client/ChatForm.Designer.cs
+++ Client/ChatForm.Designer.cs
@@ -6,7 +6,6 @@
 
         private System.Windows.Forms.Panel panelLeft;
         private System.Windows.Forms.Panel panelCenter;
-        private System.Windows.Forms.Panel panelRight;
 
         private System.Windows.Forms.FlowLayoutPanel luongChatItems;
         private System.Windows.Forms.Label labelChatTitle;
@@ -18,13 +17,6 @@
         private System.Windows.Forms.Panel panelInputBar;
         private System.Windows.Forms.TextBox oNhapTin;
         private System.Windows.Forms.Button nutGui;
-
-        private System.Windows.Forms.Label tieuDeChiTiet;
-        private System.Windows.Forms.Label labelOptions;
-        private System.Windows.Forms.Label labelMute;
-        private System.Windows.Forms.Label labelCopyLink;
-        private System.Windows.Forms.Label labelManageApps;
-        private System.Windows.Forms.Label labelLeave;
 
         protected override void Dispose(bool disposing)
         {
@@ -49,19 +41,11 @@
             this.oNhapTin = new System.Windows.Forms.TextBox();
             this.nutGui = new System.Windows.Forms.Button();
             this.tieuDeChat = new System.Windows.Forms.Label();
-            this.panelRight = new System.Windows.Forms.Panel();
-            this.labelLeave = new System.Windows.Forms.Label();
-            this.labelManageApps = new System.Windows.Forms.Label();
-            this.labelCopyLink = new System.Windows.Forms.Label();
-            this.labelMute = new System.Windows.Forms.Label();
-            this.labelOptions = new System.Windows.Forms.Label();
-            this.tieuDeChiTiet = new System.Windows.Forms.Label();
             this.panelLeft.SuspendLayout();
             this.panelSearchContainer.SuspendLayout();
             this.panelCenter.SuspendLayout();
             this.panelInputBar.SuspendLayout();
             this.inputBackground.SuspendLayout();
-            this.panelRight.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelLeft
@@ -135,7 +119,7 @@
             this.panelCenter.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelCenter.Location = new System.Drawing.Point(260, 0);
             this.panelCenter.Name = "panelCenter";
-            this.panelCenter.Size = new System.Drawing.Size(885, 622);
+            this.panelCenter.Size = new System.Drawing.Size(822, 622);
             this.panelCenter.TabIndex = 0;
             // 
             // khungTinNhan
@@ -148,7 +132,7 @@
             this.khungTinNhan.Location = new System.Drawing.Point(0, 40);
             this.khungTinNhan.Name = "khungTinNhan";
             this.khungTinNhan.Padding = new System.Windows.Forms.Padding(16, 10, 16, 10);
-            this.khungTinNhan.Size = new System.Drawing.Size(885, 512);
+            this.khungTinNhan.Size = new System.Drawing.Size(822, 512);
             this.khungTinNhan.TabIndex = 0;
             this.khungTinNhan.WrapContents = false;
             // 
@@ -162,7 +146,7 @@
             this.panelInputBar.Location = new System.Drawing.Point(0, 552);
             this.panelInputBar.Name = "panelInputBar";
             this.panelInputBar.Padding = new System.Windows.Forms.Padding(16, 10, 16, 10);
-            this.panelInputBar.Size = new System.Drawing.Size(885, 70);
+            this.panelInputBar.Size = new System.Drawing.Size(822, 70);
             this.panelInputBar.TabIndex = 1;
             // 
             // inputBackground
@@ -212,98 +196,17 @@
             this.tieuDeChat.Location = new System.Drawing.Point(0, 0);
             this.tieuDeChat.Name = "tieuDeChat";
             this.tieuDeChat.Padding = new System.Windows.Forms.Padding(14, 0, 0, 0);
-            this.tieuDeChat.Size = new System.Drawing.Size(885, 40);
+            this.tieuDeChat.Size = new System.Drawing.Size(822, 40);
             this.tieuDeChat.TabIndex = 2;
             this.tieuDeChat.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // panelRight
-            // 
-            this.panelRight.BackColor = System.Drawing.Color.White;
-            this.panelRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelRight.Controls.Add(this.labelLeave);
-            this.panelRight.Controls.Add(this.labelManageApps);
-            this.panelRight.Controls.Add(this.labelCopyLink);
-            this.panelRight.Controls.Add(this.labelMute);
-            this.panelRight.Controls.Add(this.labelOptions);
-            this.panelRight.Controls.Add(this.tieuDeChiTiet);
-            this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panelRight.Location = new System.Drawing.Point(1145, 0);
-            this.panelRight.Name = "panelRight";
-            this.panelRight.Size = new System.Drawing.Size(256, 622);
-            this.panelRight.TabIndex = 1;
-            // 
-            // labelLeave
-            // 
-            this.labelLeave.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.labelLeave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(68)))), ((int)(((byte)(55)))));
-            this.labelLeave.Location = new System.Drawing.Point(24, 185);
-            this.labelLeave.Name = "labelLeave";
-            this.labelLeave.Size = new System.Drawing.Size(180, 20);
-            this.labelLeave.TabIndex = 0;
-            this.labelLeave.Text = "Leave";
-            // 
-            // labelManageApps
-            // 
-            this.labelManageApps.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.labelManageApps.ForeColor = System.Drawing.Color.Black;
-            this.labelManageApps.Location = new System.Drawing.Point(24, 160);
-            this.labelManageApps.Name = "labelManageApps";
-            this.labelManageApps.Size = new System.Drawing.Size(180, 20);
-            this.labelManageApps.TabIndex = 1;
-            this.labelManageApps.Text = "Manage apps";
-            // 
-            // labelCopyLink
-            // 
-            this.labelCopyLink.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.labelCopyLink.ForeColor = System.Drawing.Color.Black;
-            this.labelCopyLink.Location = new System.Drawing.Point(24, 135);
-            this.labelCopyLink.Name = "labelCopyLink";
-            this.labelCopyLink.Size = new System.Drawing.Size(180, 20);
-            this.labelCopyLink.TabIndex = 2;
-            this.labelCopyLink.Text = "Copy link to chat";
-            // 
-            // labelMute
-            // 
-            this.labelMute.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.labelMute.ForeColor = System.Drawing.Color.Black;
-            this.labelMute.Location = new System.Drawing.Point(24, 110);
-            this.labelMute.Name = "labelMute";
-            this.labelMute.Size = new System.Drawing.Size(180, 20);
-            this.labelMute.TabIndex = 3;
-            this.labelMute.Text = "Mute";
-            // 
-            // labelOptions
-            // 
-            this.labelOptions.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
-            this.labelOptions.ForeColor = System.Drawing.Color.Black;
-            this.labelOptions.Location = new System.Drawing.Point(14, 80);
-            this.labelOptions.Name = "labelOptions";
-            this.labelOptions.Size = new System.Drawing.Size(200, 24);
-            this.labelOptions.TabIndex = 4;
-            this.labelOptions.Text = "Options";
-            // 
-            // tieuDeChiTiet
-            // 
-            this.tieuDeChiTiet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tieuDeChiTiet.Dock = System.Windows.Forms.DockStyle.Top;
-            this.tieuDeChiTiet.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
-            this.tieuDeChiTiet.ForeColor = System.Drawing.Color.Black;
-            this.tieuDeChiTiet.Location = new System.Drawing.Point(0, 0);
-            this.tieuDeChiTiet.Name = "tieuDeChiTiet";
-            this.tieuDeChiTiet.Padding = new System.Windows.Forms.Padding(14, 0, 0, 0);
-            this.tieuDeChiTiet.Size = new System.Drawing.Size(254, 40);
-            this.tieuDeChiTiet.TabIndex = 5;
-            this.tieuDeChiTiet.Text = "People ";
-            this.tieuDeChiTiet.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // ChatForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(246)))), ((int)(((byte)(250)))));
-            this.ClientSize = new System.Drawing.Size(1401, 622);
+            this.ClientSize = new System.Drawing.Size(1082, 622);
             this.Controls.Add(this.panelCenter);
-            this.Controls.Add(this.panelRight);
             this.Controls.Add(this.panelLeft);
             this.MinimumSize = new System.Drawing.Size(900, 500);
             this.Name = "ChatForm";
@@ -316,7 +219,6 @@
             this.panelInputBar.ResumeLayout(false);
             this.inputBackground.ResumeLayout(false);
             this.inputBackground.PerformLayout();
-            this.panelRight.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
