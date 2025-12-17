d501edfb860ffd97331ed96b4dd77e350c51e8d5
yurii <yuriismylife123@gmail.com>
2025-12-17 17:01:01 +0700

    schedule

---
+++ Client/ScheduleForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace NT106_BT2
+{
+    partial class ScheduleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelCalendar = new System.Windows.Forms.Panel();
+            this.btnToday = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.lblMonthYear = new System.Windows.Forms.Label();
+            this.lvTasks = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(1198, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 38);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Task List";
+            // 
+            // panelCalendar
+            // 
+            this.panelCalendar.Location = new System.Drawing.Point(19, 127);
+            this.panelCalendar.Name = "panelCalendar";
+            this.panelCalendar.Size = new System.Drawing.Size(1016, 791);
+            this.panelCalendar.TabIndex = 27;
+            // 
+            // btnToday
+            // 
+            this.btnToday.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnToday.Location = new System.Drawing.Point(444, 65);
+            this.btnToday.Name = "btnToday";
+            this.btnToday.Size = new System.Drawing.Size(125, 56);
+            this.btnToday.TabIndex = 26;
+            this.btnToday.Text = "Today";
+            this.btnToday.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnNext.Location = new System.Drawing.Point(898, 65);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(125, 56);
+            this.btnNext.TabIndex = 25;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnPrev.Location = new System.Drawing.Point(19, 65);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(125, 56);
+            this.btnPrev.TabIndex = 24;
+            this.btnPrev.Text = "Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // lblMonthYear
+            // 
+            this.lblMonthYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblMonthYear.Location = new System.Drawing.Point(99, 10);
+            this.lblMonthYear.Name = "lblMonthYear";
+            this.lblMonthYear.Size = new System.Drawing.Size(815, 52);
+            this.lblMonthYear.TabIndex = 23;
+            this.lblMonthYear.Text = "MONTH YEAR";
+            this.lblMonthYear.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lvTasks
+            // 
+            this.lvTasks.HideSelection = false;
+            this.lvTasks.Location = new System.Drawing.Point(1041, 64);
+            this.lvTasks.Name = "lvTasks";
+            this.lvTasks.Size = new System.Drawing.Size(471, 521);
+            this.lvTasks.TabIndex = 22;
+            this.lvTasks.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(1038, 607);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 38);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Task Decription";
+            // 
+            // txtTask
+            // 
+            this.txtTask.Location = new System.Drawing.Point(1045, 648);
+            this.txtTask.Multiline = true;
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(474, 162);
+            this.txtTask.TabIndex = 31;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnAdd.Location = new System.Drawing.Point(1392, 816);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(127, 62);
+            this.btnAdd.TabIndex = 30;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnDelete.Location = new System.Drawing.Point(1045, 816);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(127, 62);
+            this.btnDelete.TabIndex = 29;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // ScheduleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1531, 929);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panelCalendar);
+            this.Controls.Add(this.btnToday);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.lblMonthYear);
+            this.Controls.Add(this.lvTasks);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ScheduleForm";
+            this.Text = "ScheduleForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panelCalendar;
+        private System.Windows.Forms.Button btnToday;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Label lblMonthYear;
+        private System.Windows.Forms.ListView lvTasks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ Client/ScheduleForm.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NT106_BT2
+{
+
+
+    public partial class ScheduleForm : Form
+    {
+        private DateTime currentMonth = DateTime.Today;
+        private DateTime selectedDate = DateTime.Today;
+
+        public class StudentTask
+        {
+            public string Title { get; set; }
+            public bool Completed { get; set; }
+            public DateTime Date { get; set; }
+        }
+        public ScheduleForm()
+        {
+            InitializeComponent();
+
+            currentMonth = DateTime.Today;
+            selectedDate = DateTime.Today;
+
+            TaskStorage.LoadFromFile();
+
+            SetupListView();
+            DrawCalendar();
+            LoadTasks();
+        }
+        private void DrawCalendar()
+        {
+            panelCalendar.Controls.Clear();
+            lblMonthYear.Text = currentMonth.ToString("MMMM yyyy");
+
+            DateTime firstDay = new DateTime(currentMonth.Year, currentMonth.Month, 1);
+            int offset = (int)firstDay.DayOfWeek;
+            DateTime startDate = firstDay.AddDays(-offset);
+
+            int cols = 7;
+            int rows = 6;
+            int w = panelCalendar.Width / cols;
+            int h = panelCalendar.Height / rows;
+
+            for (int i = 0; i < 42; i++)
+            {
+                DateTime date = startDate.AddDays(i);
+
+                Button btn = new Button
+                {
+                    Width = w - 4,
+                    Height = h - 4,
+                    Left = (i % cols) * w,
+                    Top = (i / cols) * h,
+                    Text = date.Day.ToString(),
+                    Tag = date,
+                    TextAlign = ContentAlignment.TopLeft
+                };
+
+                if (date.Date == DateTime.Today)
+                    btn.BackColor = Color.LightGreen;
+                else if (date.Date == selectedDate)
+                    btn.BackColor = Color.LightBlue;
+                else if (date.Month != currentMonth.Month)
+                    btn.BackColor = Color.LightGray;
+                else
+                    btn.BackColor = Color.White;
+
+                if (TaskStorage.GetTasks(date).Count > 0)
+                    btn.Text += "\n●";
+
+                btn.Click += Day_Click;
+
+                panelCalendar.Controls.Add(btn);
+            }
+        }
+
+        private void Day_Click(object sender, EventArgs e)
+        {
+            selectedDate = (DateTime)((Button)sender).Tag;
+            DrawCalendar();
+            LoadTasks();
+        }
+
+        private void Day_DoubleClick(object sender, EventArgs e)
+        {
+            selectedDate = (DateTime)((Button)sender).Tag;
+            LoadTasks();
+            txtTask.Focus();
+        }
+
+        private void btnToday_Click(object sender, EventArgs e)
+        {
+            currentMonth = DateTime.Today;
+            selectedDate = DateTime.Today;
+            DrawCalendar();
+            LoadTasks();
+        }
+        private void SetupListView()
+        {
+            lvTasks.View = View.Details;
+            lvTasks.CheckBoxes = true;
+            lvTasks.Columns.Add("Task", 250);
+        }
+
+        private void LoadTasks()
+        {
+            lvTasks.Items.Clear();
+
+            foreach (var task in TaskStorage.GetTasks(selectedDate))
+            {
+                lvTasks.Items.Add(new ListViewItem(task));
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTask.Text))
+                return;
+
+            TaskStorage.AddTask(selectedDate, txtTask.Text);
+
+            txtTask.Clear();
+            LoadTasks();
+            DrawCalendar();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lvTasks.CheckedItems.Count == 0)
+                return;
+            for (int i = lvTasks.CheckedItems.Count - 1; i >= 0; i--)
+            {
+                ListViewItem item = lvTasks.CheckedItems[i];
+                string task = item.Text;
+
+                TaskStorage.RemoveTask(selectedDate, task);
+            }
+
+            LoadTasks();
+            DrawCalendar();
+        }
+        private void lvTasks_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            e.Item.Checked = e.IsSelected;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            int month = currentMonth.Month;
+            int year = currentMonth.Year;
+
+            if (month == 12)
+            {
+                month = 1;
+                year += 1;
+            }
+            else
+            {
+                month += 1;
+            }
+
+            currentMonth = new DateTime(year, month, 1);
+
+            DrawCalendar();
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            int month = currentMonth.Month;
+            int year = currentMonth.Year;
+
+            if (month == 1)
+            {
+                month = 12;
+                year -= 1;
+            }
+            else
+            {
+                month -= 1;
+            }
+
+            currentMonth = new DateTime(year, month, 1);
+
+            DrawCalendar();
+        }
+    }
+
+}
